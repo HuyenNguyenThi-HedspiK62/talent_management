5594df41c6da1d80507ce31a702b8c0951ee387f
Đức Trung <ductrung.2112xx@gmail.com>
2021-11-27 01:20:16 +0700

    hide sidebar

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -53,12 +53,12 @@
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
+    @if(auth()->user()->role == 0)
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="index3.html" class="brand-link">
             <span class="brand-text font-weight-light">タレント管理</span>
         </a>
-        @if(auth()->user()->role == 0)
         <!-- Sidebar -->
         <div class="sidebar">
             <!-- Sidebar Menu -->
@@ -85,8 +85,8 @@
             <!-- /.sidebar-menu -->
         </div>
         <!-- /.sidebar -->
-        @endif
     </aside>
+    @endif
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
