ebd9325abbedbf68ce807b5b110f9236a227c172
Đức Trung <ductrung.2112xx@gmail.com>
2021-12-07 05:56:29 +0700

    fix title

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <title>タレント管理</title>
+    <title>タレント管理ツール</title>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <!-- Font Awesome Icons -->
