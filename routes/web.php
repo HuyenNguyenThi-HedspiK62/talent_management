1136293c9e1f3ccd200618f78eaec0a5d5e8bef8
Đức Trung <ductrung.2112xx@gmail.com>
2022-01-04 17:57:40 +0700

    fix home

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\Auth\LoginController::class, 'showLoginForm']);
+Route::get('/', function (){
+    return view('welcome');
+});
 
 Auth::routes();
 
